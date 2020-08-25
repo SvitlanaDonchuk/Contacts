20017ef90a38ee15f178d96f20655e6791bb3c88
SvitlanaDonchuk <sveta.donchuk@gmail.com>
2020-08-25 23:49:46 +0300

    Added some functions

--- src/com/contacts/Main.java
+++ src/com/contacts/Main.java
@@ -7,8 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        List<Human> humanList = new ArrayList<>();
-        List<Organization> organizationList = new ArrayList<>();
+        List<BaseClass> list = new ArrayList<>();
 
         Action menuType;
         Type type;
@@ -21,33 +20,34 @@ public class Main {
 
             switch (menuType){
                 case ADD:
+
                     System.out.print("Enter the type (person, organization): ");
                     type = Type.findTypeByLabel(scanner.next());
 
                     switch (type){
                         case PERSON:
-                            functionAddHuman(humanList);
+                            functionAddHuman(list);
                             break;
                         case ORGANIZATION:
-                            functionAddOrganization(organizationList);
+                            functionAddOrganization(list);
                             break;
                     }
                     break;
 
                 case REMOVE:
-                    functionRemove(humanList);
+                    functionRemove(list);
                     break;
 
                 case EDIT:
-                    functionEdit(humanList);
+                    functionHumanEdit(list);
                     break;
 
                 case COUNT:
-                    functionCount(humanList);
+                    functionCount(list);
                     break;
 
                 case INFO:
-                    functionInfo(humanList);
+                    functionInfo(list);
                     break;
 
             }
@@ -56,12 +56,13 @@ public class Main {
 
     }
 
-    private static void functionAddHuman(List<Human> humanList ){
+    private static void functionAddHuman(List<BaseClass> list){
 
         final Scanner scanner = new Scanner(System.in);
         final Human.humanBuilder humanBuilder = new Human.humanBuilder();
 
         String regex = "^([+] ?[0-9]{1,3} ?|[0-9]{1,3} ?-?|[+]?[(][A-Za-z0-9]{1,}[)] ?)( ?-?[(]?[A-Za-z0-9]{2,3}[)]?( ?-?[A-Za-z0-9]{2,3}[)]?)?( ?-?[A-Za-z0-9]{2,3})?( ?-?[A-za-z0-9]{2,4})?)?";
+        String genderRegex = "^[mfMF]\\b";
 
         System.out.print("Enter the name: ");
         String humanName = scanner.nextLine();
@@ -70,14 +71,27 @@ public class Main {
         String humanSurname = scanner.nextLine();
 
         System.out.print("Enter the birth date: ");
-        LocalDate local = LocalDate.parse(scanner.next());
+        LocalDate local = LocalDate.parse(scanner.nextLine());
         String humanDate = local.toString();
 
+        if(humanDate.isEmpty()){
+            System.out.println("Bad birth date!");
+            humanDate = "[no data]";
+        }
+
+        System.out.print("Enter the gender (M, F): ");
+        String humanGender = scanner.nextLine();
+
+        if(humanGender.isEmpty() || !humanGender.matches(genderRegex)){
+            System.out.println("Bad gender!");
+            humanGender = "[no data]";
+        }
+
         System.out.print("Enter the number: ");
-        String humanNumber = scanner.next();
+        String humanNumber = scanner.nextLine();
 
 
-        if(humanNumber.isEmpty() || humanDate.isEmpty()){
+        if(humanNumber.isEmpty()){
             humanNumber = "[no data]";
         }
 
@@ -90,28 +104,33 @@ public class Main {
                 .setName(humanName)
                 .setSurname(humanSurname)
                 .setBirthDate(humanDate)
+                .setGender(humanGender)
                 .setNumber(humanNumber)
                 .build();
 
-        humanList.add( human);
+        list.add( human);
         System.out.println("The record added.");
 
     }
 
-    private  static void functionAddOrganization(List<Organization> organizationList) {
+    private static void functionAddOrganization(List<BaseClass> list) {
         final Scanner scanner = new Scanner(System.in);
         final Organization.organizationBuilder organizationBuilder = new Organization.organizationBuilder();
 
         String regex = "^([+] ?[0-9]{1,3} ?|[0-9]{1,3} ?-?|[+]?[(][A-Za-z0-9]{1,}[)] ?)( ?-?[(]?[A-Za-z0-9]{2,3}[)]?( ?-?[A-Za-z0-9]{2,3}[)]?)?( ?-?[A-Za-z0-9]{2,3})?( ?-?[A-za-z0-9]{2,4})?)?";
 
         System.out.print("Enter the organization name: ");
-        String organizationName = scanner.next();
+        String organizationName = scanner.nextLine();
 
         System.out.print("Enter the address: ");
-        String organizationAddress = scanner.next();
+        String organizationAddress = scanner.nextLine();
 
         System.out.print("Enter the number: ");
-        String organizationNumber = scanner.next();
+        String organizationNumber = scanner.nextLine();
+
+        if(organizationNumber.isEmpty() || !organizationNumber.matches(regex)){
+            organizationNumber = "[no data]";
+        }
 
         Organization organization = organizationBuilder
                 .setNameOfOrganization(organizationName)
@@ -119,16 +138,12 @@ public class Main {
                 .setNumber(organizationNumber)
                 .build();
 
-        organizationList.add(organization);
+        list.add(organization);
         System.out.println("The record added.");
-
-        if(organizationNumber.isEmpty() || !organizationNumber.matches(regex)){
-            organizationNumber = "[no data]";
-        }
 
     }
 
-    private static void functionRemove(List<Human> list){
+    private static void functionRemove(List<BaseClass> list){
 
         Scanner scanner = new Scanner(System.in);
 
@@ -138,7 +153,7 @@ public class Main {
         }
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.print(i + 1 + ". " + list.get(i));
+            System.out.print(i + 1 + ". " + list.get(i).getName());
         }
 
         System.out.print("Select a record: ");
@@ -151,14 +166,28 @@ public class Main {
 
     }
 
-    private static void functionInfo(List<Human> list){
+    private static void functionInfo(List<BaseClass> list){
+        Scanner scanner = new Scanner(System.in);
+        int record;
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.print(i + 1 + ". " + list.get(i));
+            System.out.println(i + 1 + ". " + list.get(i).getName());
         }
+
+        System.out.print("Select a record: ");
+        record = scanner.nextInt() - 1;
+
+        System.out.print(list.get(record));
+
     }
 
-    private static void functionEdit(List<Human> list){
+    private static void functionCount(List<BaseClass> list){
+
+        System.out.println("The Phone Book has " + list.size() + " records.");
+
+    }
+
+    private static void functionHumanEdit(List<BaseClass> list){
 
         Scanner scanner = new Scanner(System.in);
 
@@ -168,14 +197,14 @@ public class Main {
         }
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.print(i + 1 + ". " + list.get(i));
+            System.out.println(i + 1 + ". " + list.get(i).getName());
         }
 
         System.out.print("Select a record: ");
 
         int record = scanner.nextInt() - 1;
 
-        System.out.print("Select a field (name, surname, number): ");
+        System.out.print("Select a field (name, surname, birth, gender, number): ");
 
         Fields field = Fields.findFieldsByLabel(scanner.next());
 
@@ -188,6 +217,12 @@ public class Main {
                 changeSurname(list, record);
                 break;
 
+            case BIRTH:
+                changeBirth(list, record);
+
+            case GENDER:
+                changeGender(list, record);
+
             case NUMBER:
                 changeNumber(list, record);
                 break;
@@ -195,17 +230,11 @@ public class Main {
         }
     }
 
-    private static void functionCount(List<Human> list){
-
-        System.out.println("The Phone Book has " + list.size() + " records.");
-
-    }
-
-    private static  void changeName(List<Human> list, int record){
+    private static void changeName(List<BaseClass> list, int record){
 
         Scanner scanner = new Scanner(System.in);
 
-        Human human = list.get(record);
+        BaseClass human = list.get(record);
 
         System.out.print("Enter name: ");
 
@@ -217,11 +246,11 @@ public class Main {
 
     }
 
-    private static  void changeSurname(List<Human> list, int record){
+    private static void changeSurname(List<BaseClass> list, int record){
 
         Scanner scanner = new Scanner(System.in);
 
-        Human human = list.get(record);
+        Human human = (Human) list.get(record);
 
         System.out.print("Enter surname: ");
 
@@ -233,13 +262,13 @@ public class Main {
 
     }
 
-    private static  void changeNumber(List<Human> list, int record){
+    private static void changeNumber(List<BaseClass> list, int record){
 
         Scanner scanner = new Scanner(System.in);
 
         String regex = "\\s^([+] ?[0-9]{1,3} ?|[0-9]{1,3} ?-?|[+]?[(][A-Za-z0-9]{1,}[)] ?)( ?-?[(]?[A-Za-z0-9]{2,3}[)]?( ?-?[A-Za-z0-9]{2,3}[)]?)?( ?-?[A-Za-z0-9]{2,3})?( ?-?[A-za-z0-9]{2,4})?)?";
 
-        Human human = list.get(record);
+        BaseClass human = list.get(record);
 
         System.out.print("Enter number: ");
 
@@ -260,14 +289,46 @@ public class Main {
 
     }
 
+    private static void changeBirth(List<BaseClass> list, int record){
+
+        Scanner scanner = new Scanner(System.in);
+
+        Human human = (Human) list.get(record);
+
+        System.out.print("Enter Birth date: ");
+
+        String birth = scanner.next();
+
+        human.setBirth(birth);
+
+        System.out.println("The record updated!");
+    }
+
+    private static void changeGender(List<BaseClass> list, int record){
+
+        Scanner scanner = new Scanner(System.in);
+
+        Human human = (Human) list.get(record);
+
+        System.out.print("Enter gender: ");
+
+        String gender = scanner.next();
+
+        human.setGender(gender);
+
+        System.out.println("The record updated!");
+    }
+
 }
 
 abstract class BaseClass{
 
     private String number;
+    private String name;
 
-    BaseClass(String number){
+    BaseClass(String number, String name){
         this.number = number;
+        this.name = name;
     }
 
     public void setNumber(String number) {
@@ -277,24 +338,24 @@ abstract class BaseClass{
     public String getNumber() {
         return number;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
 
 class Organization extends BaseClass{
 
-    private String nameOfOrganization;
     private String address;
 
-    Organization(String number, String nameOfOrganization, String address) {
-        super(number);
-        this.nameOfOrganization = nameOfOrganization;
-        this.address = address;
-    }
-
-    public void setNameOfOrganization(String nameOfOrganization) {
-        this.nameOfOrganization = nameOfOrganization;
-    }
-
-    public void setAddress(String address) {
+    Organization(String name, String address, String number) {
+        super(number, name);
         this.address = address;
     }
 
@@ -327,7 +388,7 @@ class Organization extends BaseClass{
 
     @Override
     public String toString() {
-        return "Organization name: " + nameOfOrganization + "\n" +
+        return "Organization name: " + getName() + "\n" +
                 "Address: " + address + "\n" +
                 "Number: " + getNumber() + "\n" +
                 "Time created: " + "\n" +
@@ -339,23 +400,31 @@ class Organization extends BaseClass{
 
 class Human extends BaseClass{
 
-    private String Name;
     private String Surname;
     private String BirthDate;
+    private String gender;
 
-     Human(String name, String surname, String number, String birthDate) {
-         super(number);
-         this.Name = name;
+     Human(String name, String surname, String number, String birthDate, String gender) {
+         super(number, name);
          this.Surname = surname;
          this.BirthDate = birthDate;
+         this.gender = gender;
     }
 
-    void setName(String name) {
-         Name = name;
+    public String getGender() {
+        return gender;
     }
 
-    void setSurname(String surname) {
-         Surname = surname;
+    public void setSurname(String surname) {
+         this.Surname = surname;
+    }
+
+    public void setBirth(String birth) {
+         this.BirthDate = birth;
+    }
+
+    public void setGender(String gender) {
+         this.gender = gender;
     }
 
     static class humanBuilder {
@@ -364,6 +433,7 @@ class Human extends BaseClass{
         private String Surname;
         private String BirthDate;
         private String Number;
+        private String gender;
 
         humanBuilder setName(String name) {
             this.Name = name;
@@ -385,18 +455,23 @@ class Human extends BaseClass{
             return this;
         }
 
+        humanBuilder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
         Human build() {
-            return new Human(Name, Surname, Number, BirthDate);
+            return new Human(Name, Surname, Number, BirthDate, gender);
         }
 
     }
 
     @Override
     public String toString() {
-        return "Name: " + Name + "\n" +
+        return "Name: " + getName() + "\n" +
                 "Surname: " + Surname + "\n" +
                 "Birth date: " + BirthDate + "\n" +
-                "Gender: " + "\n" +
+                "Gender: " + getGender() + "\n" +
                 "Number: " + getNumber() + "\n" +
                 "Time created: " + "\n" +
                 "Time last edit: " + "\n";
@@ -433,6 +508,8 @@ enum Fields{
 
     NAME("name"),
     SURNAME("surname"),
+    BIRTH ("birth"),
+    GENDER("gender"),
     NUMBER("number");
 
     private String fieldLabel;
@@ -454,7 +531,7 @@ enum Fields{
 enum Type {
 
     PERSON("person"),
-    ORGANIZATION("organization");
+    ORGANIZATION("org");
 
     private String typeLabel;
 
